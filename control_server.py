148c045358a41bd48605c29baae0ee2ec0487056
Builder <builder@example.com>
2026-05-26 16:35:47 +0000

    feat(boardgamegeek): add BoardGameGeek mirror

--- control_server.py
+++ control_server.py
@@ -27,6 +27,7 @@ SITES = [
     'github', 'google_flights', 'google_map', 'google_search',
     'huggingface', 'wolfram_alpha', 'cambridge_dictionary',
     'coursera', 'espn', 'merriam_webster', 'ikea', 'phys_org', 'target', 'ted',
+    'boardgamegeek',
 ]
 BASE_PORT = 40000
 WEBSYN_DIR = '/opt/WebSyn'
